1f4916e07f09824278a70ba811a52382d7d82fbf
leodav1 <ldmujica@gmail.com>
2024-03-08 22:38:19 +0100

    empezando css

--- dato.js
+++ dato.js
@@ -1,31 +1,47 @@
 import { data } from "./poke.js";
 
-const pokedexContainer = document.getElementById("pokedex");
+const pokedexContainer = document.getElementById("pokedex-container");
 
 for (let i = 0; i < data.length; i++) {
-    const pokemon = data[i];
+    const pokemonData = data[i];
 
     const pokemonCard = document.createElement("div");
     pokemonCard.classList.add("pokemon-card");
 
+    const pokemonCardInfo = document.createElement("div");
+    pokemonCardInfo.classList.add("pokemon-card-info");
+
     const pokemonImage = document.createElement("img");
     pokemonImage.classList.add("pokemon-image");
-    pokemonImage.src = `pokemones/${pokemon.thumbnail}`;
-    pokemonImage.alt = pokemon.name;
+    pokemonImage.src = `pokemones/${pokemonData.thumbnail}`;
+    pokemonImage.alt = pokemonData.name;
 
     const pokemonName = document.createElement("p");
-    pokemonName.textContent = pokemon.name;
+    pokemonName.textContent = pokemonData.name;
 
     const pokemonId = document.createElement("p");
-    pokemonId.textContent = `id: ${pokemon.id}`;
+    pokemonId.textContent = `id: ${pokemonData.id}`;
 
-    const pokemonTypes = document.createElement("p");
-    pokemonTypes.textContent = `Type: ${pokemon.type.join(", ")}`;
+    const pokemonTypesList = document.createElement("ul");
+    pokemonTypesList.className = "pokemon-types-list";
+
+    for (let j = 0; j < pokemonData.type.length; j++) {
+        const pokemonType = pokemonData.type[j];
+
+        const pokemonTypeItem = document.createElement("li")
+        pokemonTypeItem.className=`pokemon-type-item ${pokemonType.toLowerCase()}`
+        pokemonTypeItem.textContent = pokemonType;
+
+        pokemonTypesList.appendChild(pokemonTypeItem);
+    }
 
     pokemonCard.appendChild(pokemonImage);
-    pokemonCard.appendChild(pokemonName);
-    pokemonCard.appendChild(pokemonId);
-    pokemonCard.appendChild(pokemonTypes);
+
+    pokemonCardInfo.appendChild(pokemonName);
+    pokemonCardInfo.appendChild(pokemonId);
+    pokemonCardInfo.appendChild(pokemonTypesList);
+
+    pokemonCard.appendChild(pokemonCardInfo);
 
     pokedexContainer.appendChild(pokemonCard);
 }
